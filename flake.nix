d4877399d2188a2669c7c68386c5245607eb250d
Bas van Dijk <bas@van.dijk.ch>
2025-01-14 14:53:39 +0100

    Use home-manager to manage dotfiles

--- flake.nix
+++ flake.nix
@@ -3,66 +3,112 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager }:
   let
-    configuration = { pkgs, ... }:
-      let
-        emacs = (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages (epkgs:
-          with epkgs.melpaPackages; [
-            bazel
-            magit
-            forge
-            flycheck
-            company
-            github-review
-            haskell-mode
-            zenburn-theme
-            solarized-theme
-            yaml-mode
-            markdown-mode
-            nix-mode
-            nix-sandbox
-            ormolu
-            graphviz-dot-mode
-            direnv
-            projectile
-            terraform-mode
-            go-mode
-            rust-mode
-            lsp-mode
-            lsp-ui
-            adoc-mode
-            plantuml-mode
-            use-package
-            rg
-            tt-mode
-            elixir-mode
-            yasnippet
-            hledger-mode
-          ]);
-      in {
+    configuration = { pkgs, ... }: {
+      imports = [ home-manager.darwinModules.home-manager ];
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users."bas" = {
+          home.stateVersion = "24.11";
+          programs.home-manager.enable = true;
+
+          programs.direnv = {
+            enable = true;
+            nix-direnv.enable = true;
+          };
+
+          programs.git = {
+            enable = true;
+            userEmail = "bas@van.dijk.ch";
+            userName = "Bas van Dijk";
+          };
+
+          programs.kitty = {
+            enable = true;
+          };
+
+          programs.oh-my-posh = {
+            enable = true;
+            useTheme = "M365Princess";
+          };
+
+          programs.zsh = {
+            enable = true;
+            syntaxHighlighting.enable = true;
+            shellAliases = {
+              ls = "ls --color";
+              l = "ls --color -la";
+              t = "terraform";
+              k = "kubectl";
+              h = "hledger";
+            };
+          };
+
+          programs.emacs = {
+            enable = true;
+            extraPackages = epkgs: with epkgs; [
+              bazel
+              magit
+              forge
+              flycheck
+              company
+              github-review
+              haskell-mode
+              zenburn-theme
+              solarized-theme
+              yaml-mode
+              markdown-mode
+              nix-mode
+              nix-sandbox
+              ormolu
+              graphviz-dot-mode
+              direnv
+              projectile
+              terraform-mode
+              go-mode
+              rust-mode
+              lsp-mode
+              lsp-ui
+              adoc-mode
+              plantuml-mode
+              use-package
+              rg
+              tt-mode
+              elixir-mode
+              yasnippet
+              hledger-mode
+            ];
+          };
+        };
+      };
+
+      users.users."bas" = {
+        home = "/Users/bas";
+      };
+
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
         coreutils
-        git
         git-filter-repo
         gh
         htop
-        zsh
-        oh-my-zsh
         tree
         gnupg
         openssl
         plantuml
         ripgrep
         jq
-        direnv
-        nix-direnv
         graphviz
         cabal2nix
         cabal-install
@@ -73,16 +119,15 @@
         ormolu
         tldr
         nodejs_22
-        kitty
-
         pkgs.haskellPackages.ghc
       ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+      fonts.packages = [
+        # See: https://www.nerdfonts.com/font-downloads
+        pkgs.nerd-fonts._0xproto
+      ];
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      nix.settings.experimental-features = "nix-command flakes";
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -91,12 +136,7 @@
       # $ darwin-rebuild changelog
       system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
-
-      programs.direnv.enable = true;
-
-      programs.zsh.enableSyntaxHighlighting = true;
     };
   in
   {
